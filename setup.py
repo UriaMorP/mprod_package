772c5a5507932381319860b96f4fde76346594b4
UriaMorP <UriaMorP@users.noreply.github.com>
2021-11-14 11:26:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
 configuration = {
-    "name":'mprod_package',
-    "version":'0.1dev',
+    "name":'mprod-package',
+    "version":'0.0.1',
     "packages":['mprod'],
     "long_description":open('README.md').read(),
+    "extras_require": {
+        "dev":["pytest>=6.2.0",]
+    }
 }
 
 setup(**configuration)

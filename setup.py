3f362a5c65c115cf700f28261ea6fce3e1e7a619
UriaMorP <UriaMorP@users.noreply.github.com>
2021-12-01 16:46:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,16 @@ configuration = {
         "pandas >= 1.1.5"
     ],
     "extras_require": {
-        "dev":["pytest==6.2.2",]
+        "dev":["pytest==6.2.2",],
+        "docs":[
+            "sphinx-gallery == 0.9.0",
+            "numpydoc == 1.1.0",
+            "sphinxcontrib-bibtex == 2.3.0",
+            "sphinx-prompt == 1.4.0",
+            "nbsphinx == 0.8.6",
+            "sphinx_rtd_theme == 0.5.2",
+            "ipykernel == 5.4.3",
+        ]
     }
 }
 
